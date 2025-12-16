e6fd6296e7a2eaf26259eb534b93a87c68c0416e
Joseph Asare <52238457+asare-21@users.noreply.github.com>
2025-12-15 22:14:00 -0500

    added the home page to the router_config

--- lib/utils/router_config.dart
+++ lib/utils/router_config.dart
@@ -1,10 +1,16 @@
 import 'package:go_router/go_router.dart';
 import 'package:safe_zone/authentication/view/authentication.dart';
+import 'package:safe_zone/home/view/home.dart';
 
 GoRouter routerConfig = GoRouter(
   initialLocation: '/authentication',
 
   routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, state) => const HomeScreen(),
+    ),
     GoRoute(
       path: '/authentication',
       name: 'authentication',
